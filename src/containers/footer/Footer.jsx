791b770e6631edc7e63a689d2d6cf1072ec18c58
Nishant Kumar <nishantkumar222555@gmail.com>
2022-08-22 23:43:33 +0530

    first commit

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Nishant Kumar <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>nishantkumar222555@gmail.com</p>
+        <p>+91 730 263 0238</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@20UCS116 NISHANT_KUMAR @ NIT AGARTALA</p>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
